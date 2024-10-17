a2f90607cd9fa7add661f4252fcbd48e9559c18a
Matheus Peniche <mathz.programador19@gmail.com>
2024-10-17 08:40:08 -0300

    Primeiro commit

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade é do seu carro é de ${vel} km/h`)
+if( vel > 60){ // condição simples! 
+    console.log(`Você ultrapassou o limite da velocidade permitida. ESTÁ MULTADO!!`)
+}
+console.log(`Dirija sempre com cuidado e com o cinto de segurança!`)
---
+++ ex009.js
@@ -0,0 +1,8 @@
+var país = 'brasil'
+
+console.log(`vivendo em ${país}`)
+    if( país == 'Brasil'){
+        console.log('Vc é Brasileiro')
+    } else {
+        console.log('vc é estrangeiro')
+    }
